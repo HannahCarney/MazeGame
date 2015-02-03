e79238d1a7b24c0ec5fe5906b5c2fc305cb610ff
Oliver <o.lucas@live.com>
2015-02-03 15:32:49 +0000

    2 tests passing

--- spec/homepageSpec.js
+++ spec/homepageSpec.js
@@ -1,14 +1,21 @@
 var Player = require('../js/player');
+var Path = require('../js/path');
 
 
 describe('By default', function(){
 	var player
+  var path
   
   beforeEach(function(){
   	player = new Player();
+    path = new Path();
   });
 
   it('should have a starting point', function() {
     expect(player.begin).toBe("a1");
+  });
+
+  it('should have a path', function(){
+    expect(path.grid).toContain("b2");
   });
 });
